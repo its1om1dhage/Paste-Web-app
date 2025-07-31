7f6f670ba6be83cf81ec5fc780ad9f957ff4a296
its1om1dhage <dhagesoham513@gmail.com>
2025-07-31 15:35:05 +0530

    React redux basic setup done

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './Styles/main.css'
+import { store } from './store.js'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit'
+import pasteReducer from './Redux/pasteSlice'
+
+export const store = configureStore({
+  reducer: {},
+})
