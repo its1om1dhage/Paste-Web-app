ba7f26bff324ad4f2430e2893180d92d5f60d4ef
its1om1dhage <dhagesoham513@gmail.com>
2025-07-31 17:42:30 +0530

    reducer is set in redux and finally basic redux withot logic is ready

--- src/store.js
+++ src/store.js
@@ -2,5 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import pasteReducer from './Redux/pasteSlice'
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+     paste: pasteReducer,
+  },
 })
